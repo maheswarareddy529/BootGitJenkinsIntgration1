9bcb17467aa65d085b1718188fa1ff01ab73f69d
maheswara <maheswarareddy529@gmail.com>
2022-01-19 18:14:41 +0530

    initial commit

---
+++ src/main/java/com/example/demo/BootGitJenkinsIntegration1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGitJenkinsIntegration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGitJenkinsIntegration1Application.class, args);
+	}
+
+}
